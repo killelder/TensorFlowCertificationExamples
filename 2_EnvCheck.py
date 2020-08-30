6b894f15473105df7be5240320be542c1a07cda6
Kled <wayne7chen24@gmail.com>
2020-08-30 10:41:11 +0800

    [Environment Check]
    To check installation for practice or for Exam is completed or not

---
+++ 2_EnvCheck.py
@@ -0,0 +1,95 @@
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+import tensorflow_datasets as tfds
+
+tfds.disable_progress_bar()
+
+embedding_layer = layers.Embedding(1000, 5)
+result = embedding_layer(tf.constant([1, 2, 3]))
+result.numpy()
+
+result = embedding_layer(tf.constant([[0, 1, 2], [3, 4, 5]]))
+result.shape
+(train_data, test_data), info = tfds.load(
+    'imdb_reviews/subwords8k',
+    split=(tfds.Split.TRAIN, tfds.Split.TEST),
+    with_info=True, as_supervised=True)
+encoder = info.features['text'].encoder
+encoder.subwords[:20]
+train_batches = train_data.shuffle(1000).padded_batch(10)
+test_batches = test_data.shuffle(1000).padded_batch(10)
+train_batch, train_labels = next(iter(train_batches))
+train_batch.numpy()
+embedding_dim = 16
+
+model = keras.Sequential([
+    layers.Embedding(encoder.vocab_size, embedding_dim),
+    layers.GlobalAveragePooling1D(),
+    layers.Dense(16, activation='relu'),
+    layers.Dense(1)
+])
+
+model.summary()
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+history = model.fit(
+    train_batches,
+    epochs=10,
+    validation_data=test_batches, validation_steps=20)
+import matplotlib.pyplot as plt
+
+history_dict = history.history
+
+acc = history_dict['accuracy']
+val_acc = history_dict['val_accuracy']
+loss = history_dict['loss']
+val_loss = history_dict['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.figure(figsize=(12, 9))
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+plt.figure(figsize=(12, 9))
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+plt.ylim((0.5, 1))
+plt.show()
+e = model.layers[0]
+weights = e.get_weights()[0]
+print(weights.shape)  # shape: (vocab_size, embedding_dim)
+import io
+
+encoder = info.features['text'].encoder
+
+out_v = io.open('vecs.tsv', 'w', encoding='utf-8')
+out_m = io.open('meta.tsv', 'w', encoding='utf-8')
+
+for num, word in enumerate(encoder.subwords):
+    vec = weights[num + 1]  # skip 0, it's padding.
+    out_m.write(word + "\n")
+    out_v.write('\t'.join([str(x) for x in vec]) + "\n")
+out_v.close()
+out_m.close()
+try:
+    from google.colab import files
+except ImportError:
+    pass
+else:
+    files.download('vecs.tsv')
+    files.download('meta.tsv')
